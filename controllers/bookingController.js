0698d0c481bad609c8e0b72121c425754d6df9e4
krsanjay11 <kumarsanjay77888@gmail.com>
2024-03-28 13:02:00 +0530

    live image in payment page

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -30,7 +30,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`], // live image
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+            ], // live image
           },
         },
       },
